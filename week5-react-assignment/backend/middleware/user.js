1aea4f3d4a9886f36b6ff6d075bab4ca26844af5
Pranav Jinturkar <pranavjinturkar452@gmail.com>
2025-09-28 18:12:16 +0530

    Middleware Done for both user and admin

---
+++ week5-react-assignment/backend/middleware/user.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config({ quiet: true });
+
+export default function authenticateAdmin(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer "))
+    return res.status(401).json({
+      message: "Unauthorized Access",
+      success: false,
+    });
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decodedValue = jwt.verify(token, process.env.JWT_SECRET);
+    req.less = {
+      username: decodedValue.username,
+      type: "user",
+    };
+    next();
+  } catch (error) {
+    console.log(error, error.message);
+    res.status(403).json({
+      message: "Unauthorized",
+      success: false,
+    });
+  }
+}
